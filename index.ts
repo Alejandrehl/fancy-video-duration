c0090b42c091b82c25e3b7899f5f18ff539b327c
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-03-15 10:09:41 -0300

    save

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello");
+import { getDurationFromBuffer } from "./utils/get-duration-from-buffer";
+import { fancyTimeFormat } from "./utils/fancy-time-format";
+
+const getVideoDuration = (buffer: Buffer) => {
+  const durationLength = getDurationFromBuffer(buffer);
+  const fancyTimeFormatDuration = fancyTimeFormat(durationLength);
+
+  return fancyTimeFormatDuration;
+};
+
+module.exports = {
+  getVideoDuration,
+  getDurationFromBuffer,
+  fancyTimeFormat,
+};
